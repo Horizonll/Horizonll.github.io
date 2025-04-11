985a393670cd71145f23ac9ae66ed4d745f2dd5e
Horizonll <120229787+Horizonll@users.noreply.github.com>
2025-04-11 09:19:45 +0000

    Deploying to gh-pages from @ Horizonll/Horizonll.github.io@74d0713ea884e8d72498036c261caa9d2a1b9cfc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,51 +72,16 @@ ninja.data = [{
           section: "News",},{id: "news-thmos-reached-the-quarter-finals-in-the-humanoid-league-kid-size-and-5-th-place-in-the-adultsize-league-at-robocup-2024",
           title: 'THMOS reached the quarter-finals in the Humanoid League Kid Size and 5-th place...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ai-generated-facial-image-authentication",
+          title: 'AI-Generated Facial Image Authentication',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-pillpalpro-a-multifunctional-smart-pill-box",
+          title: 'PillPalPro - A Multifunctional Smart Pill Box',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
